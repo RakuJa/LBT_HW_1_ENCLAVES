fef748dae5bd4a809afa254a78a9b5b3212e742a
TheAsel <theasel99@gmail.com>
2023-04-17 16:32:32 +0200

    updated ast and added interpreter skeleton

--- enclave/lib/ast.ml
+++ enclave/lib/ast.ml
@@ -1,21 +1,44 @@
 open Env
-
+ 
+type identifier = string
+type ty =
+  | Nint of int
+  | Nbool of bool
+  | Nstring of string
+  | Nunit of unit
+  (* ... other possible types ... *)
+  
+ 
+type func_type =
+  (* Function arguments (0..n), return type *)
+  | FuncType of ty * ty
+ 
 type expr =
   | Nint of int
   | Nbool of bool
   | Nstring of string
-  | Var of ide
-  | Let of ide * expr * expr
-  | SecLet of ide * expr * expr
-  | Prim of ide * expr * expr
+  | Nunit of unit
+  | Var of identifier
+  | Let of identifier * expr * expr
+  (* Tipes for the primitives, +, - etc*)
+  | Prim of identifier * expr * expr
   | If of expr * expr * expr
-  | Fun of ide * expr
+  (* Lambda: parameters *)
+  | Fun of identifier * expr
   | Call of expr * expr
-  | Enclave of ide * expr
-  | Gateway of ide * expr
-  | IncludeUntrusted of ide * expr
-  | ExecuteUntrusted of string
-
-type value =
-  | Int of int
-  | Closure of ide * expr * value env
+    (* EncLet type, id, variable value, body *)
+  | EncLet of ty * identifier * expr * expr
+ 
+  (* It's a list because we can separate all the expressions inside for further analysis*)
+  | Enclave of identifier * expr list
+  (* Type, Function name, argument_name, expr *)
+  | Gateway of func_type * identifier * string * expr
+  | IncludeUntrusted of identifier * expr
+  | ExecuteUntrusted of expr
+ 
+(*
+  A runtime value is an integer or a function closure
+  Boolean are encoded as integers.
+*)
+type value = Int of int | String of string | Unit of unit | Closure of identifier * expr * value env
+ 
--- enclave/lib/interpreter.ml
+++ enclave/lib/interpreter.ml
@@ -1,2 +1,60 @@
 open Ast
 open Env
+
+let rec eval (e : expr) (env : value env) : value =
+  match e with
+  | Nint i -> Int i
+  | Nbool b -> Int (if b then 1 else 0)
+  | Nstring s -> String s
+  | Nunit u -> Unit u
+  | Var x -> lookup env x
+  | Let (x, eRhs, letBody) ->
+      let xVal = eval eRhs env in
+      let letEnv = (x, xVal) :: env in
+      eval letBody letEnv
+  | Prim (ope, e1, e2) -> (
+    let v1 = eval e1 env in
+    let v2 = eval e2 env in
+    match (ope, v1, v2) with
+    | "*", Int i1, Int i2 -> Int (i1 * i2)
+    | "+", Int i1, Int i2 -> Int (i1 + i2)
+    | "-", Int i1, Int i2 -> Int (i1 - i2)
+    | "=", Int i1, Int i2 -> Int (if i1 = i2 then 1 else 0)
+    | ">", Int i1, Int i2 -> Int (if i1 > i2 then 1 else 0)
+    | "<", Int i1, Int i2 -> Int (if i1 < i2 then 1 else 0)
+    | _ -> failwith "unknown primitive or wrong type")
+  | If (e1, e2, e3) -> (
+    match eval e1 env with
+    | Int 0 -> eval e3 env
+    | Int _ -> eval e2 env
+    | _ -> failwith "eval if")
+  | Fun (x, fBody) -> Closure (x, fBody, env)
+  | Call (eFun, eArg) -> (
+    let fClosure = eval eFun env in
+    match fClosure with
+    | Closure (x, fBody, fDeclEnv) ->
+        let xVal = eval eArg env in
+        let fBodyEnv = (x, xVal) :: fDeclEnv in
+        eval fBody fBodyEnv
+    | _ -> failwith "eval Call: not a function")
+  | EncLet (ty, x, eRhs, letBody) ->
+    (*TODO*)
+    let xVal = eval eRhs env in
+    let letEnv = (x, xVal) :: env in
+    eval letBody letEnv
+  | Enclave (x, eList) ->
+    (*TODO*)
+    let x = 1 in
+    eval x
+  | Gateway (funType, x, funName, eGate) ->
+    (*TODO*)
+    let x = 1 in
+    eval x
+  | IncludeUntrusted (x, eInc) ->
+    (*TODO*)
+    let x = 1 in
+    eval x
+  | ExecuteUntrusted (eExe) ->
+    (*TODO*)
+    let x = 1 in
+    eval x
