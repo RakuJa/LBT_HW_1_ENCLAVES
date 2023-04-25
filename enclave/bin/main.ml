325b9277b123fef3dcba3b9fda1104f317a4e142
TheAsel <theasel99@gmail.com>
2023-04-26 00:10:43 +0200

    more tests

--- enclave/bin/main.ml
+++ enclave/bin/main.ml
@@ -5,29 +5,79 @@ let _test_gateway_1 = Enclave (
   "myEnclave",
    SecLet (
     "password", CstI(5),
-    Gateway ("password", CstI(5), EndEnclave)
+   Gateway ("password", CstI(5), EndEnclave)
   ), CstI(7)
 )
 
 let _test_gateway_2 = Enclave (
   "myEnclave",
+  SecLet (
+   "x", CstI (1),
    SecLet (
     "f", 
     Fun (
-      "x",
-      Prim ("+", Var "x", CstI 1)
+      "y",
+      Prim ("+", Var ("x") , CstI 1)
     ) 
     ,
-    Gateway ("password", Var ("f"), EndEnclave)
-  ), CstI(7)
+   Gateway ("myGateway", Var ("f"), EndEnclave)
+  )), EnCall ("myEnclave", "myGateway", CstI (1))
 )
 
 let _test_gateway_3 = Enclave (
   "myEnclave",
    SecLet (
+   "password", CstI(5),
+   Gateway ("myGateway", Fun ("leak", Var ("password")), EndEnclave)
+  ), EnCall ("myEnclave", "myGateway", CstI (1))
+)
+
+let _test_gateway_4 = Gateway 
+   ("myGateway", Fun ("outside", Var ("password")), CstI (1))
+
+let _test_enclave_1 = Enclave (
+   "myEnclave1",
+   SecLet (
     "password", CstI(5),
-    Gateway ("password", Fun ("leak", Var ("password")), EndEnclave)
+   Enclave ("myEnclave2", CstI(5), EndEnclave)
   ), CstI(7)
 )
 
-let _value_1 = eval _test_gateway_3 [] []
+let _test_enclave_2 = Enclave (
+  "myEnclave", CstI (1), CstI (2)
+)
+
+let _test_untrusted_1 = IncludeUntrusted (
+   CstI (1), EndUntrusted
+)
+
+let _test_untrusted_2 = IncludeUntrusted (
+   IncludeUntrusted (CstI (1), EndUntrusted), EndUntrusted
+)
+
+let _test_untrusted_3 = Enclave (
+  "myEnclave",
+  SecLet (
+   "x", CstI (1),
+   SecLet (
+    "f", 
+    Fun (
+      "y",
+      Prim ("+", Var ("x") , CstI 1)
+    ) 
+    ,
+   Gateway ("myGateway", Var ("f"), EndEnclave)
+  )), IncludeUntrusted (EnCall ("myEnclave", "myGateway", CstI (1)), EndUntrusted
+))
+
+(*
+let _test_1 = eval _test_gateway_1 [] []
+let _test_2 = eval _test_gateway_2 [] []
+let _test_3 = eval _test_gateway_3 [] []
+let _test_4 = eval _test_gateway_4 [] []
+let _test_5 = eval _test_enclave_1 [] []
+let _test_6 = eval _test_enclave_2 [] []
+let _test_7 = eval _test_untrusted_1 [] []
+let _test_8 = eval _test_untrusted_2 [] []
+let _test_9 = eval _test_untrusted_3 [] []
+*)
