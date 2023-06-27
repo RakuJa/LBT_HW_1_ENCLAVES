730b8c088f8eaf870905e43579844d51fb911557
RakuJa <danielegiachetto1999@gmail.com>
2023-06-27 22:50:29 +0200

    remove useless sec lvl, refactor untrusted sec lvl

--- enclave/lib/env.ml
+++ enclave/lib/env.ml
@@ -15,8 +15,8 @@ type sec_level =
   For simplicity we represent the environment as an association list, i.e., a list of (identifier, data, sec_level).
 *)
 
-type 'v env = (ide * 'v * sec_level) list
-type 'v enclave = {secrets: (ide * 'v * sec_level) list; generics: (ide * 'v * sec_level) list; gateways: (ide * 'v * sec_level) list}
+type 'v env = (ide * 'v ) list
+type 'v enclave = {secrets: (ide * 'v ) list; generics: (ide * 'v ) list; gateways: (ide * 'v) list}
 
 (*
   Given an environment {env} and an identifier {x} it returns the data {x} is bound to and its security level {sec}.
@@ -25,4 +25,4 @@ type 'v enclave = {secrets: (ide * 'v * sec_level) list; generics: (ide * 'v * s
 let rec lookup env x: 'v =
   match env with
   | [] -> failwith (x ^ " not found")
-  | (y, v, sec) :: r -> if x = y then (v, sec) else lookup r x
+  | (y, v) :: r -> if x = y then v else lookup r x
--- enclave/lib/interpreter.ml
+++ enclave/lib/interpreter.ml
@@ -7,23 +7,15 @@ let rec eval_gateway (e: expr) (env : value env) (secrets : value env) : value =
   | CstB (b, sec) -> Int ((if b then 1 else 0), sec)
   | Var x -> (
       try 
-        let v, _ = lookup secrets x in v
+        lookup secrets x
 
       with Not_found -> 
-        let v, _ = lookup env x in v
+        lookup env x
     )
   | Let (x, eRhs, letBody) -> (
       let xVal = eval_gateway eRhs env secrets in
-      match (xVal) with
-      | Int (_, sec) -> (
-          let letEnv = (x, xVal, sec) :: env in
-          eval_gateway letBody letEnv secrets
-        )
-      | _ -> (
-          let letEnv = (x, xVal, Low) :: env in
-          eval_gateway letBody letEnv secrets
-        )
-
+      let letEnv = (x, xVal) :: env in
+      eval_gateway letBody letEnv secrets
     )
   (*if either operand is high-security, the result is high-security; otherwise, it is marked as low-security*)
   | Prim (ope, e1, e2) -> (
@@ -86,15 +78,8 @@ let rec eval_gateway (e: expr) (env : value env) (secrets : value env) : value =
       match fClosure with
       | Closure (x, fBody, fDeclEnv) -> (
           let xVal = eval_gateway eArg env secrets in
-          match xVal with
-          | Int (_, High) -> (
-              let fBodyEnv = (x, xVal, High) :: fDeclEnv in
-              eval_gateway fBody fBodyEnv secrets
-            )
-          | _ -> (
-              let fBodyEnv = (x, xVal, Low) :: fDeclEnv in
-              eval_gateway fBody fBodyEnv secrets  
-            )
+          let fBodyEnv = (x, xVal) :: fDeclEnv in
+          eval_gateway fBody fBodyEnv secrets
         )
       | _ -> failwith "eval Call: not a function"
     )
@@ -115,22 +100,39 @@ let rec eval_gateway (e: expr) (env : value env) (secrets : value env) : value =
 
 let rec eval_untrusted (e : expr) (env : value env) : value =
   match e with
-  | CstI (i, sec) -> Int (i, sec)
-  | CstB (b, sec) -> Int ((if b then 1 else 0), sec)
-  | Var x -> let v, _ = lookup env x in v
+  | CstI (i, sec) -> (
+      match(sec) with 
+      | High -> failwith "Cannot have high value inside untrusted code"
+      | _ -> Int (i, Low)
+    )
+  | CstB (b, sec) -> (
+      match(sec) with 
+      | High -> failwith "Cannot have high value inside untrusted code"
+      | _ -> Int ((if b then 1 else 0), Low)
+    ) 
+  | Var x -> lookup env x
   | Let (x, eRhs, letBody) ->
     let xVal = eval_untrusted eRhs env in
-    let letEnv = (x, xVal, Low) :: env in
+    let letEnv = (x, xVal) :: env in
     eval_untrusted letBody letEnv
   | Prim (ope, e1, e2) -> (
       let v1 = eval_untrusted e1 env in
       let v2 = eval_untrusted e2 env in
       match (ope, v1, v2) with
-      | "*", Int (i1, _), Int (i2, _) -> Int (i1 * i2, Low)
-      | "+", Int (i1, _), Int (i2, _) -> Int (i1 + i2, Low)
-      | "-", Int (i1, _), Int (i2, _) -> Int (i1 - i2, Low)
-      | "=", Int (i1, _), Int (i2, _) -> Int ((if i1 = i2 then 1 else 0), Low)
-      | "<", Int (i1, _), Int (i2, _) -> Int ((if i1 < i2 then 1 else 0), Low)
+      | "*", Int (i1, Low), Int (i2, Low) -> Int(i1 * i2, Low)
+      | "*", _, _ -> failwith "Cannot use a high value in untrusted code" 
+
+      | "+", Int (i1, Low), Int (i2, Low) -> Int(i1 + i2, Low)
+      | "+", _, _ -> failwith "Cannot use a high value in untrusted code" 
+
+      | "-", Int (i1, Low), Int (i2, Low) -> Int(i1 - i2, Low)
+      | "-", _, _ -> failwith "Cannot use a high value in untrusted code"  
+
+      | "=", Int (i1, Low), Int (i2, Low) -> Int((if i1 = i2 then 1 else 0), Low)
+      | "=", _, _ -> failwith "Cannot use a high value in untrusted code"  
+
+      | "<", Int (i1, Low), Int (i2, Low) -> Int((if i1 < i2 then 1 else 0), Low)
+      | "<", _, _ -> failwith "Cannot use a high value in untrusted code"  
       | _ -> failwith "unknown primitive or wrong type"
     )
   | If (cond, e2, e3) -> (
@@ -146,7 +148,7 @@ let rec eval_untrusted (e : expr) (env : value env) : value =
       | Closure (x, fBody, fDeclEnv) ->
         (* xVal is evaluated in the current stack *)
         let xVal = eval_untrusted eArg env in
-        let fBodyEnv = (x, xVal, Low) :: fDeclEnv in
+        let fBodyEnv = (x, xVal) :: fDeclEnv in
 
         (* fBody is evaluated in the updated stack *)
         eval_untrusted fBody fBodyEnv
@@ -161,15 +163,14 @@ let rec eval_untrusted (e : expr) (env : value env) : value =
   | Gateway(_, _, _) -> failwith "Gateway let is not allowed outside of an Enclave. Abort." 
   | IncludeUntrusted(_, _) -> failwith "Cannot reference untrusted code from untrusted code. Abort."
 
-(*WIP: aggiunto sec_level a encl_list, discutibile*)
-let rec eval (e : expr) (env : value env) (encl_list : (ide * value enclave * sec_level) list) : value =
+let rec eval (e : expr) (env : value env) (encl_list : (ide * value enclave) list) : value =
   match e with
   | CstI (i, sec) -> Int (i, sec)
   | CstB (b, sec) -> Int ((if b then 1 else 0), sec)
-  | Var x -> let v, _ = lookup env x in v
+  | Var x -> lookup env x
   | Let (x, eRhs, letBody) ->
     let xVal = eval eRhs env encl_list in
-    let letEnv = (x, xVal, High) :: env in
+    let letEnv = (x, xVal) :: env in
     eval letBody letEnv encl_list
   | Prim (ope, e1, e2) -> (
       let v1 = eval e1 env encl_list in
@@ -195,7 +196,7 @@ let rec eval (e : expr) (env : value env) (encl_list : (ide * value enclave * se
       | "<", Int (i1, High), Int (i2, High) -> Int((if i1 < i2 then 1 else 0), High)
       | "<", Int (i1, Low), Int (i2, Low) -> Int((if i1 < i2 then 1 else 0), Low)
       | "<", _, _ -> failwith "Cannot use a low value together with a high value" 
-      
+
 
       | _ -> failwith "unknown primitive or wrong type"
     )
@@ -227,14 +228,14 @@ let rec eval (e : expr) (env : value env) (encl_list : (ide * value enclave * se
     )
   | Fun (x, fBody) -> Closure (x, fBody, env)
   | EnCall (enclIde, gatewayIde, encParams) -> (
-      let req_enclave, _ = lookup encl_list enclIde in
-      let req_gateway, _ = lookup req_enclave.gateways gatewayIde in
+      let req_enclave = lookup encl_list enclIde in
+      let req_gateway = lookup req_enclave.gateways gatewayIde in
       match req_gateway with
       | EnClosure (x, expr, secrets, generics, _) -> (
           let encParValues = eval encParams env encl_list in
           match encParValues with
           | Int(_, High) -> (
-              let fBodyEnv = (x, encParValues, High) :: env in
+              let fBodyEnv = (x, encParValues) :: env in
               match eval_gateway expr (generics @ fBodyEnv) secrets with 
               | eval_gt_result -> 
                 match (eval_gt_result) with
@@ -247,7 +248,7 @@ let rec eval (e : expr) (env : value env) (encl_list : (ide * value enclave * se
                 | _ -> failwith "Operation not allowed! Abort."
             )
           | _ -> (
-              let fBodyEnv = (x, encParValues, Low) :: env in
+              let fBodyEnv = (x, encParValues) :: env in
               match eval_gateway expr (generics @ fBodyEnv) secrets with 
               | eval_gt_result -> 
                 match (eval_gt_result) with
@@ -268,15 +269,8 @@ let rec eval (e : expr) (env : value env) (encl_list : (ide * value enclave * se
       | Closure (x, fBody, fDeclEnv) -> (
           (* xVal is evaluated in the current stack *)
           let xVal = eval eArg env encl_list in
-          match xVal with
-          | Int (_, High) -> (
-              let fBodyEnv = (x, xVal, High) :: fDeclEnv in
-              eval fBody fBodyEnv encl_list
-            )
-          | _ -> (
-              let fBodyEnv = (x, xVal, Low) :: fDeclEnv in
-              eval fBody fBodyEnv encl_list  
-            )
+          let fBodyEnv = (x, xVal) :: fDeclEnv in
+          eval fBody fBodyEnv encl_list
         )
       | _ -> failwith "eval Call: not a function"
     )
@@ -286,7 +280,7 @@ let rec eval (e : expr) (env : value env) (encl_list : (ide * value enclave * se
       let result = eval_enclave enclBody [][][] in
       match result with
       | Renclave (enclaveRes) -> (
-          let encList = (x, enclaveRes, High) :: encl_list in
+          let encList = (x, enclaveRes) :: encl_list in
           eval nextExpr env encList
         )
       | _ -> failwith "Wrong return type from enclave, maybe you are missing an EndEnclave?")
@@ -315,17 +309,17 @@ and eval_enclave (e : expr) (secrets : value env) (generics : value env) (gatewa
   | Var x -> enclave_lookup secrets generics gateways x
   | Let (x, eRhs, letBody) ->
     let xVal = eval_enclave eRhs secrets generics gateways in
-    let letEnv = (x, xVal, High) :: generics in
+    let letEnv = (x, xVal) :: generics in
     eval_enclave letBody secrets letEnv gateways
   | SecLet (x, eRhs, letBody) ->
     let xVal = eval_enclave eRhs secrets generics gateways in
-    let letEnv = (x, xVal, High) :: secrets in
+    let letEnv = (x, xVal) :: secrets in
     eval_enclave letBody letEnv generics gateways
   | Gateway (x, eRhs, letBody) ->  (
       let xVal = eval_enclave eRhs secrets generics gateways in 
       match xVal with
       | EnClosure(_, _, secrets, generics, gateways) -> (
-          let letEnv = (x, xVal, High) :: gateways in
+          let letEnv = (x, xVal) :: gateways in
           eval_enclave letBody secrets generics letEnv
         )
       | _ -> failwith "eval gateway: not a function. A let Gateway must be a Closure! Abort."
@@ -388,15 +382,8 @@ and eval_enclave (e : expr) (secrets : value env) (generics : value env) (gatewa
       | EnClosure (x, fBody, fDeclSec, fDeclGen, fDeclGat) -> (
           (* xVal is evaluated in the current stack *)
           let xVal = eval_enclave eArg secrets generics gateways in
-          match xVal with
-          | Int (_, High) -> (
-              let fBodyEnv = (x, xVal, High) :: fDeclGen in
-              eval_enclave fBody fDeclSec fBodyEnv fDeclGat
-            )
-          | _ -> (
-              let fBodyEnv = (x, xVal, Low) :: fDeclGen in
-              eval_enclave fBody fDeclSec fBodyEnv fDeclGat  
-            )
+          let fBodyEnv = (x, xVal) :: fDeclGen in
+          eval_enclave fBody fDeclSec fBodyEnv fDeclGat
         )
       | _ -> failwith "eval Call: not a function"
     )
@@ -414,9 +401,9 @@ and eval_enclave (e : expr) (secrets : value env) (generics : value env) (gatewa
 
 and enclave_lookup (secrets : value env) (generics : value env) (gateways : value env) x : value =
   try 
-    let v, _ = lookup secrets x in v
+    lookup secrets x
   with Not_found ->
   try
-    let v, _ = lookup generics x in v
+    lookup generics x
   with Not_found -> 
-    let v, _  = lookup gateways x in v
+    lookup gateways x
